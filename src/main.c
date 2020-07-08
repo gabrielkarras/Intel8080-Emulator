fb568d17cd0791b7b9e503aea403f14c7dd047b1
gabrielkarras <gavrilkarras@hotmail.com>
2020-07-08 01:06:48 -0400

    Updated emulator

--- src/main.c
+++ src/main.c
@@ -30,6 +30,7 @@ typedef struct States {
 
 void IncompleteInstruction(States *state);
 int Emulator(States *state);
+int Parity(uint16_t x);
 
 
 int main(int argc, char **argv){
@@ -38,11 +39,23 @@ int main(int argc, char **argv){
 }
 
 
+/*
+  For any unimplmented instruction in the Intel8080 instruction set
+  We will simply exit the emulation(halt)
+*/
 void IncompleteInstruction(States *state)
 {
   state->pc -= 1; // undoing PC increment
   printf("Error: Incomplete Instrction\n");
   EXIT_FAILURE;
+}
+
+/*
+  Tests wether an input is even or odd parity bit
+*/
+int Parity(uint16_t x)
+{
+  return 0;
 }
 
 int Emulator(States *state)
@@ -64,8 +77,47 @@ int Emulator(States *state)
     case 0x42: state->b = state->d; break;// MOV B,D
     case 0x43: state->b = state->e; break;// MOVE B,E
     /* ... */
-    case 0xFE: IncompleteInstruction(state); break;
-    case 0xFF: IncompleteInstruction(state); break;
+    case 0x80: // ADD B
+        {
+          uint16_t answer = (uint16_t)state->a + (uint16_t)state->b;
+          state->cc.z = ((answer & 0xff) == 0);// If result is zero, set flag
+          state->cc.s = ((answer & 0x80) != 0);// If bit 7 is set, set sign flag
+          state->cc.cy = (answer > 0xff);// Set carry flag if exceeds 2 bytes
+          state->cc.p = Parity(answer & 0xff);// Parity flag is set if even
+          state->a = answer & 0xff;
+        } break;
+    case 0x81: // ADD C
+        {
+          uint16_t answer = (uint16_t)state->a + (uint16_t)state->c;
+          state->cc.z = ((answer & 0xff) == 0);
+          state->cc.s = ((answer & 0x80) != 0);
+          state->cc.cy = (answer > 0xff);
+          state->cc.p = Parity(answer & 0xff);
+          state->a = answer & 0xff;
+        } break;
+    case 0x86: // ADD M
+        {
+          uint16_t offset = (state->h<<8) | (state->l);
+          uint16_t answer = (uint16_t)state->a +
+                            (uint16_t)state->memory[offset];
+          state->cc.z = ((answer & 0xff) == 0);
+          state->cc.s = ((answer & 0x80) != 0);
+          state->cc.cy = (answer > 0xff);
+          state->cc.p = Parity(answer & 0xff);
+          state->a = answer & 0xff;
+        } break;
+    case 0xc2:
+    case 0xc6: // ADI byte
+        {
+          uint16_t answer = (uint16_t)state->a + (uint16_t)opcode[1];
+          state->cc.z = ((answer & 0xff) == 0);
+          state->cc.s = ((answer & 0x80) != 0);
+          state->cc.cy = (answer > 0xff);
+          state->cc.p = Parity(answer & 0xff);
+          state->a = answer & 0xff;
+        } break;
+    case 0xfe: IncompleteInstruction(state); break;
+    case 0xff: IncompleteInstruction(state); break;
   }
   state->pc += 1;
 }
